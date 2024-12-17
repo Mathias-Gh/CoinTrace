df810283a282b841cd7f83bdaedeea8aed17b02a
kayreddd <148087656+kayreddd@users.noreply.github.com>
2024-12-17 02:19:38 +0100

    add form of login and register

--- app/src/main/java/com/example/cointrace/ui/compte/CompteFragment.kt
+++ app/src/main/java/com/example/cointrace/ui/compte/CompteFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.cointrace.databinding.FragmentCompteBinding
@@ -28,9 +29,24 @@ class CompteFragment : Fragment() {
         _binding = FragmentCompteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCompte
+        val textView: TextView = binding.loginText
         compteViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+
+        // Gestion du clic sur le bouton de connexion
+        binding.loginButton.setOnClickListener {
+            val username = binding.username.text.toString()
+            val password = binding.password.text.toString()
+
+            // Vérifiez les identifiants
+            if (username == "user" && password == "1234") {
+                binding.loginText.text = "Connexion réussie !"
+                Toast.makeText(requireContext(), "Connexion réussie !", Toast.LENGTH_SHORT).show()
+            } else {
+                binding.loginText.text = "Échec de la connexion !"
+                Toast.makeText(requireContext(), "Échec de la connexion !", Toast.LENGTH_SHORT).show()
+            }
         }
         return root
     }
--- app/src/main/java/com/example/cointrace/ui/compte/CompteViewModel.kt
+++ app/src/main/java/com/example/cointrace/ui/compte/CompteViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CompteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "Login"
     }
     val text: LiveData<String> = _text
 }
